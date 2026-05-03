97bd7a2d32d3cbdf229b87034e4746507d89c2ec
Laukpindang <ferdiansyahbandung3@gmail.com>
2026-05-03 15:53:26 +0700

    first commit

---
+++ TP_MODUL10_103022400053/Controllers/FilmController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_MODUL10_103022400053;
+
+namespace TP_MODUL10_103022400053.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : Controller
+    {
+        private static readonly List<Film> films = new List<Film>
+        {
+            new Film
+            {
+                Judul = "Inception",
+                Sutradara = "Christopher Nolan",
+                Tahun = "2010",
+                Genre = "Sci-Fi",
+                Rating = "9.0"
+            },
+            new Film
+            {
+                Judul = "Interstellar",
+                Sutradara = "Christopher Nolan",
+                Tahun = "2014",
+                Genre = "Sci-Fi",
+                Rating = "8.7"
+            },
+            new Film
+            {
+                Judul = "Parasite",
+                Sutradara = "Bong Joon-ho",
+                Tahun = "2019",
+                Genre = "Thriller",
+                Rating = "8.6"
+            }
+        };
+        [HttpGet]
+        public IEnumerable<Film> Get()
+        {
+            return films;
+        }
+
+        [HttpGet("{id}")]
+        public Film Get(int id)
+        {
+            return films[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Film filmBaru)
+        {
+            films.Add(filmBaru);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            films.RemoveAt(id);
+        }
+    }
+
+}
